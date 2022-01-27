5dc59a4138599f6d43919f1772eb11231cf24abf
Lejoly Florent <florent.lejoly@inmanta.com>
2022-01-27 14:12:31 +0100

    add ci-integration-tests

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -13,7 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 
-    Contact: inmanta@inmanta.com
+    Contact: code@inmanta.com
 """
 from inmanta.plugins import plugin
 
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -13,7 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 
-    Contact: inmanta@inmanta.com
+    Contact: code@inmanta.com
 """
 from pytest_inmanta.plugin import Project
 
